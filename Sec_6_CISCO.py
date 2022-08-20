8a70d9562258d86e4c20a881d92d97919ac98202
Soumava-221B <soumavadas06@gmail.com>
2022-08-20 13:37:21 +0530

    some maths program

---
+++ Sec_6_CISCO.py
@@ -0,0 +1,11 @@
+# anything = input("Tell me anything... ")
+# print("Hmm...", anything, "...Really?")
+
+# The result of the input() function
+# anything = int(input("Enter a number: "))
+# something = anything ** 2
+# print(anything, "to the power of 2 is", something)
+
+#  Replication
+# The * (asterisk) sign, when applied to a string and number (or a number and string, as it remains commutative in this position) becomes a replication operator:
+
